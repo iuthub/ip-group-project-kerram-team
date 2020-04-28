e340bdffd82ef795b42b312ff45a6f625c36a4f1
github_inhanis <anis1999k@gmail.com>
2020-04-28 22:43:08 +0500

    some changes on views templates

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.index');
+});
+
+Route::get('/about', function () {
+	return view('pages.about');
+});
+
+Route::get('/contact', function () {
+	return view('pages.contact');
 });
