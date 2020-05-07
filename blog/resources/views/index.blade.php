2e95f4d0697a403a9ffd9ceec45bdff5d805c252
Javokhir_Ziyautdinov_u1810141 <Javokhir_Ziyautdinov_u1810141@users.noreply.github.com>
2020-05-07 10:29:15 +0500

    Blog
    basement

--- blog/resources/views/index.blade.php
+++ blog/resources/views/index.blade.php
@@ -12,15 +12,77 @@
   </head>
   <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
 <a href="#" class="navbar-brand">
 <img src="Logo.png"
 width="108" height="30" alt=logo>
-
 </a>
-
+<button class="navbar-toggler" type="button" data-toggle="collapse"
+data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+aria-expanded="false" aria-label="Toggle navigation"  >
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse justify-content-md-end" id="navbarSupportedContent">
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+      <a href="#" class="nav-link">Home </a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">Services</a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">Sing In</a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">Make Order</a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link">About US</a>
+    </li>
+</ul>
+</div>
     </nav>
 
+
+    <div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">
+
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="img-fluid" src="pic1.png" alt="First Slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="img-fluid" src="pic2.png" alt="Second Slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="img-fluid" src="pic3.png" alt="Third Slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </div>
+            </div>
+        </div>
+
+        <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </a>
+    </div>
 
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
