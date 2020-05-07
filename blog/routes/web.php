a08fac5a8f65164ef5964e3950a330415a751e3f
Dostonbek Rustamov <dostonbekrustamov20@gmail.com>
2020-05-07 21:02:21 +0500

    Addition of "Contact Us" Page
    Added "Contact Page" HTML code and style CSS

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,7 +1,11 @@
 <?php
-
-
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
+});
+
+Route::get('/contact', function () {
+	return view('others.contact_us');
 });
