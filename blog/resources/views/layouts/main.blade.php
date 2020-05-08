720120af11223c4b981db1f10b0f605818d3323e
Javokhir_Ziyautdinov_u1810141 <Javokhir_Ziyautdinov_u1810141@users.noreply.github.com>
2020-05-08 21:21:55 +0500

    Bad Coomit
    Bad commit

---
+++ blog/resources/views/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Hotel</title>
+        <!-- Fonts -->
+      <!--  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css"> -->
+        <!-- Styles -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        <div class="content">
+            @include('layouts.navbar')
+            @yield('content')
+            @include('layouts.footer')
+        </div>
+    </body>
+</html>
