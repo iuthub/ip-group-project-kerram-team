d1d0681789246ff02a1c4ecffb6f1ee0c96e74a1
Javokhir_Ziyautdinov_u1810141 <Javokhir_Ziyautdinov_u1810141@users.noreply.github.com>
2020-05-09 16:34:55 +0500

    ds
    ds

--- blog/resources/views/others/contact_us.blade.php
+++ blog/resources/views/others/contact_us.blade.php
@@ -5,7 +5,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="stylesheet" href="/css/contact_us.css"/>
-		
+
 		<title>Our Hotel</title>
 	</head>
 	<body>
