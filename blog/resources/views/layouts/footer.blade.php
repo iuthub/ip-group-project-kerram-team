c7490c8c44045730c81757cf7305507c61298621
github_inhanis <anis1999k@gmail.com>
2020-05-10 16:29:26 +0500

    minor changes

--- blog/resources/views/layouts/footer.blade.php
+++ blog/resources/views/layouts/footer.blade.php
@@ -15,20 +15,20 @@ footer {
       <div class="col-6 col-md">
         <h5>About</h5>
         <ul class="list-unstyled text-small">
-          <li><a class="text-white" href="{{route('Gallery')}}#">History</a></li>
-          <li><a class="text-white" href="{{route('Gallery')}}#">Facilities</a></li>
-          <li><a class="text-white" href="{{route('Gallery')}}#">Restaurant</a></li>
+          <li><a class="text-white" href="{{route('MainPage')}}#OurHistory">History</a></li>
+          <li><a class="text-white" href="{{route('MainPage')}}#OurFacilities">Facilities</a></li>
+          <li><a class="text-white" href="{{route('Gallery')}}#OurFacilities">Restaurant</a></li>
           <li><a class="text-white" href="{{route('About')}}#">Stuff</a></li>
-          <li><a class="text-white" href="{{route('Gallery')}}#">Fitness</a></li>
+          <li><a class="text-white" href="{{route('Gallery')}}#OurFacilities">Fitness</a></li>
         </ul>
       </div>
       <div class="col-6 col-md">
         <h5>Gallery</h5>
         <ul class="list-unstyled text-small">
           <li><a class="text-white" href="{{route('Booking')}}#">Rooms</a></li>
-          <li><a class="text-white" href="{{route('Gallery')}}#">Restaurant</a></li>
+          <li><a class="text-white" href="{{route('MainPage')}}#OurFacilities">Restaurant</a></li>
           <li><a class="text-white" href="{{route('Gallery')}}#">Our Guests</a></li>
-          <li><a class="text-white" href="{{route('MainPage')}}#">Location</a></li>
+          <li><a class="text-white" href="{{route('MainPage')}}#OurLocation">Location</a></li>
         </ul>
       </div>
       <div class="col-6 col-md">
