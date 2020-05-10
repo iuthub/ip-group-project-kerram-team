0a69aeb950ba165c9a5535ae86a5aa96e0676e60
github_inhanis <anis1999k@gmail.com>
2020-05-10 17:32:10 +0500

    Debugging

--- blog/resources/views/layouts/footer.blade.php
+++ blog/resources/views/layouts/footer.blade.php
@@ -16,35 +16,29 @@ footer {
       <div class="col-6 col-md">
         <h5>About</h5>
         <ul class="list-unstyled text-small">
-<<<<<<< HEAD
           <li><a class="text-white" href="{{route('Rooms')}}#">History</a></li>
           <li><a class="text-white" href="{{route('Rooms')}}#">Facilities</a></li>
           <li><a class="text-white" href="{{route('Rooms')}}#">Restaurant</a></li>
           <li><a class="text-white" href="{{route('About')}}#">Stuff</a></li>
           <li><a class="text-white" href="{{route('Rooms')}}#">Fitness</a></li>
-=======
           <li><a class="text-white" href="{{route('MainPage')}}#OurHistory">History</a></li>
           <li><a class="text-white" href="{{route('MainPage')}}#OurFacilities">Facilities</a></li>
           <li><a class="text-white" href="{{route('Gallery')}}#OurFacilities">Restaurant</a></li>
           <li><a class="text-white" href="{{route('About')}}#">Stuff</a></li>
           <li><a class="text-white" href="{{route('Gallery')}}#OurFacilities">Fitness</a></li>
->>>>>>> bf8894e66d2af9706f9df5dabd9774df17441d0c
         </ul>
       </div>
       <div class="col-6 col-md">
         <h5>Gallery</h5>
         <ul class="list-unstyled text-small">
-<<<<<<< HEAD
           <li><a class="text-white" href="{{route('Rooms')}}#">Rooms</a></li>
           <li><a class="text-white" href="{{route('Rooms')}}#">Restaurant</a></li>
           <li><a class="text-white" href="{{route('Rooms')}}#">Our Guests</a></li>
           <li><a class="text-white" href="{{route('MainPage')}}#">Location</a></li>
-=======
           <li><a class="text-white" href="{{route('Booking')}}#">Rooms</a></li>
           <li><a class="text-white" href="{{route('MainPage')}}#OurFacilities">Restaurant</a></li>
           <li><a class="text-white" href="{{route('Gallery')}}#">Our Guests</a></li>
           <li><a class="text-white" href="{{route('MainPage')}}#OurLocation">Location</a></li>
->>>>>>> bf8894e66d2af9706f9df5dabd9774df17441d0c
         </ul>
       </div>
       <div class="col-6 col-md">
